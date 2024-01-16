c39301a91141c0fae92fd6abf349ed4914104cd4
Lisa Daudibon <lisa.daudibon@outlook.fr>
2024-01-16 11:13:20 +0100

    :sparkles: adding the fizzbuzz method with description comment - Step 2 TDD

--- src/main/java/com/zenika/zacademy/Main.java
+++ src/main/java/com/zenika/zacademy/Main.java
@@ -3,10 +3,42 @@ package com.zenika.zacademy;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.print("Hello and welcome!");
+    /**
+     * Méthode permettant de gérer l'algorithme de Fizzbuzz
+     * <ul>
+     * <li>« Fizz » à la place du nombre si celui-ci est divisible par 3.</li>
+     * <li>« Buzz » à la place du nombre si celui-ci est divisible par 5 et non par 3.</li>
+     * <li>« FizzBuzz » à la place du nombre si celui-ci est divisible à la fois par 3 et par 5.</li>
+     * <li>sinon afficher le nombre.</li>
+     * </ul>
+     * @param value
+     * @return
+     */
+    public static String getFizzBuzzValue ( int value) {
+        checkIfValueIsBetween1And100(value);
 
+        if ( value % 15 == 0 ) {
+            return "FizzBuzz";
+        } else if ( value % 5 == 0 ){
+            return "Buzz";
+        } else if ( value % 3 == 0 ) {
+            return "Fizz";
+        }
+
+        return String.valueOf(value);
+    }
+
+    private static void checkIfValueIsBetween1And100(int value) throws RuntimeException {
+        if (value < 1 || value > 100) {
+            String exceptionMessage = value < 1 ?
+                    "La valeur doit être supérieur à 0 !" :
+                    "La valeur doit être inférieur à 101 !";
+            throw new RuntimeException(exceptionMessage);
+        }
+    }
+
+    public static void main(String[] args) {
+    getFizzBuzzValue(8);
+    getFizzBuzzValue(15);
     }
 }
--- src/test/java/com/zenika/zacademy/MainTest.java
+++ src/test/java/com/zenika/zacademy/MainTest.java
@@ -1,55 +1,64 @@
 package com.zenika.zacademy;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.junit.jupiter.api.Nested;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static com.zenika.zacademy.Main.getFizzBuzzValue;
 
 class MainTest {
     // Cas passant
-    @Test
-    void shouldReturnFizzIfValueDivisibleBy3() {
-        String result = getFizzBuzzValue(9);
-        assertEquals("Fizz", result);
-    }
+    @Nested
+    class CheckPassingCases {
+        @Test
+        void shouldReturnFizzIfValueDivisibleBy3() {
+            String result = getFizzBuzzValue(9);
+            assertEquals("Fizz", result);
+        }
 
-    @Test
-    void shouldReturnBuzzIfValueDivisibleBy5() {
-        String result = getFizzBuzzValue(10);
-        assertEquals("Buzz", result);
-    }
+        @ParameterizedTest
+        @ValueSource(ints = {5, 10, 20, 25, 35})
+        void shouldReturnBuzzIfValueDivisibleBy5(int currentValue) {
+            String result = getFizzBuzzValue(currentValue);
+            assertEquals("Buzz", result);
+        }
 
-    @Test
-    void shouldReturnFizzBuzzIfValueDivisibleBy15() {
-        String result = getFizzBuzzValue(15);
-        assertEquals("FizzBuzz", result);
-    }
 
-    @Test
-    void shouldReturnInputIfOtherwise() {
-        int inputNumber = 8;
-        String result = getFizzBuzzValue(inputNumber);
-        assertEquals(String.valueOf(inputNumber), result);
+        @ParameterizedTest
+        @ValueSource(ints = {15, 30, 45, 60, 75, 90})
+        void shouldReturnFizzBuzzIfValueDivisibleBy15(int currentValue) {
+            String result = getFizzBuzzValue(currentValue);
+            assertEquals("FizzBuzz", result);
+        }
+
+        @Test
+        void shouldReturnInputIfOtherwise() {
+            int inputNumber = 8;
+            String result = getFizzBuzzValue(inputNumber);
+            assertEquals(String.valueOf(inputNumber), result);
+        }
     }
-    //Cas non passant
+//    Cas non passant
 //    @Test
 //    void shouldThrowExceptionIfValueIsNotNumber() {
 //        assertThrows(Exception.class, () -> {});
 //    }
 
     @Test
-    void shouldThrowExceptionifValueInferiorTo1() {
-        assertThrows(Exception.class, () -> {
+    void shouldThrowExceptionIfValueInferiorTo1() {
+        Exception exception = assertThrows(Exception.class, () -> {
             getFizzBuzzValue(0);
         });
-
+        assertEquals("La valeur doit être supérieur à 0 !", exception.getMessage());
     }
 
     @Test
     void shouldThrowExceptionIfValueIsSuperiorTo100() {
-        assertThrows(Exception.class, () -> {
+        Exception exception = assertThrows(Exception.class, () -> {
             getFizzBuzzValue(105);
         });
-
+        assertEquals("La valeur doit être inférieur à 101 !", exception.getMessage());
     }
 }
