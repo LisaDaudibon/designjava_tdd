2217dac6235895a5f71bab8fbbdd144ab2074ef6
Lisa Daudibon <lisa.daudibon@outlook.fr>
2024-01-16 11:11:47 +0100

    :white_check_mark: Updating the test - TDD

--- src/test/java/com/zenika/zacademy/MainTest.java
+++ src/test/java/com/zenika/zacademy/MainTest.java
@@ -3,41 +3,53 @@ package com.zenika.zacademy;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static com.zenika.zacademy.Main.getFizzBuzzValue;
 
 class MainTest {
     // Cas passant
     @Test
     void shouldReturnFizzIfValueDivisibleBy3() {
+        String result = getFizzBuzzValue(9);
         assertEquals("Fizz", result);
     }
 
     @Test
     void shouldReturnBuzzIfValueDivisibleBy5() {
-
+        String result = getFizzBuzzValue(10);
+        assertEquals("Buzz", result);
     }
 
     @Test
     void shouldReturnFizzBuzzIfValueDivisibleBy15() {
-
+        String result = getFizzBuzzValue(15);
+        assertEquals("FizzBuzz", result);
     }
 
     @Test
     void shouldReturnInputIfOtherwise() {
-
+        int inputNumber = 8;
+        String result = getFizzBuzzValue(inputNumber);
+        assertEquals(String.valueOf(inputNumber), result);
     }
     //Cas non passant
-    @Test
-    void shouldThrowExceptionIfValueIsNotNumber() {
-
-    }
+//    @Test
+//    void shouldThrowExceptionIfValueIsNotNumber() {
+//        assertThrows(Exception.class, () -> {});
+//    }
 
     @Test
     void shouldThrowExceptionifValueInferiorTo1() {
+        assertThrows(Exception.class, () -> {
+            getFizzBuzzValue(0);
+        });
 
     }
 
     @Test
     void shouldThrowExceptionIfValueIsSuperiorTo100() {
+        assertThrows(Exception.class, () -> {
+            getFizzBuzzValue(105);
+        });
 
     }
 }
